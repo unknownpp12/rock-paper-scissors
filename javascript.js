f9c16b22a0462ddf50cdc5825861a3c343d77753
unknownpp12 <amanpokharel51@gmail.com>
2026-01-02 22:06:32 +0545

    removed previous comments

--- javascript.js
+++ javascript.js
@@ -1,24 +1,6 @@
 let humanScore= 0;
 let computerScore =0;
 let choice1="";
-/*
-function getHumanChoice(){
-    choice = prompt("What's your choice?");
-    choice= choice.toLowerCase().trim();
-    if( choice ==="rock"){
-        console.log("rock");
-    }
-    else if( choice == "paper"){
-        console.log("paper");
-    }
-    else if( choice == "scissors"){
-        console.log("scissors");
-    }
-    else {
-        console.log("put something valid.");
-
-    }
-}; */
 function getComputerChoice(){
     let num= Math.floor(Math.random()*3);
     if (num ==0){
@@ -42,33 +24,24 @@ function playRound(e){
     if(playerChoice===computerChoice){
         console.log("It's tie!");
         result.textContent="It's tie! ";
-        //result.appendChild(rel);
     }
     else if (playerChoice=='rock' && computerChoice== 'scissors'){
         console.log("You win!");  
-        // let rel= document.createTextNode("You win!  ");
-        // result.appendChild(rel);
         result.textContent="You win!  ";
         humanScore++;
     }
     else if( playerChoice=="paper"&& computerChoice=="rock"){
         console.log("You win!");
-        // let rel= document.createTextNode("You win!  ");
-        // result.appendChild(rel);
         result.textContent="You win!";
         humanScore++;
     }
     else if(playerChoice=="scissors"&& computerChoice=="paper"){
         console.log("You win!");
-        // let rel= document.createTextNode("You win!  ");
-        // result.appendChild(rel);
         result.textContent="You win!";
         humanScore++;
     }
     else{
         console.log("You lose!!");
-        // let rel= document.createTextNode("You lose!  ");
-        // result.appendChild(rel);
         result.textContent="You lose!";
         computerScore++;
     }
@@ -76,58 +49,8 @@ function playRound(e){
     humChoice.textContent=`playerChoice:${playerChoice}`;
     humScore.textContent=`humanScore:${humanScore}`;
     compScore.textContent=`computerScore:${computerScore}`;
+};
 
-    /* if(button_1.textContent=="rock" &&choice1=="rock"){
-        console.log("It's a tie");
-    }
-    else if (button_1 == "rock" && choice1=="scissors"){
-        console.log("You win!");
-        humanScore++;
-    }
-    else if( button_1 =="rock" && choice1=="paper"){
-        console.log("You lose!");
-        computerScore++;
-    }
-    else if(button_2=="paper" &&choice1=="paper"){
-        console.log("It's a tie");
-    }
-    else if ( button_2 == "paper" && choice1 == "rock"){
-        console.log("You win!");
-        humanScore++
-    }
-    else if (button_2 == "paper"&& choice1== "scissors"){
-        console.log("You lose!");
-        computerScore++;
-    }
-    else if(button_3=="scissors" &&choice1=="scissors"){
-        console.log("It's a tie");
-    }
-    else if(button_3 == "scissors"&& choice1 == "paper"){
-        console.log("You win!");
-        humanScore++;
-    }
-    else if( button_3== "scissors"&& choice1=="rock" ){
-        console.log("You lose!");
-        computerScore++;
-    } */
-};
-/* function playGame(){
-    for ( let a=0; a<=2; a++){
-        getHumanChoice();
-        if(choice==="rock" || choice === "paper"|| choice ==="scissors"){
-        getComputerChoice();
-        playRound();
-    }}
-    console.log(`You scored:`+humanScore);
-    console.log(`Computer Scored:`+computerScore)
-    if(humanScore > computerScore){
-        console.log("Congrat's! You won!");
-    }
-    else{
-        console.log("Sorry! You lost!");
-    }
-};
-playGame(); */
 let button_1=document.querySelector('.first');
 let button_2=document.querySelector('.second');
 let button_3=document.querySelector('.third');
